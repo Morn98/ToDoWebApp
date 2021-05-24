b420f916292c824a291555ff279cc35ef38a1e91
Morn98 <nentwig@mail.hs-ulm.de>
2021-05-24 18:31:34 +0200

    Add SQLite database

--- node/app.js
+++ node/app.js
@@ -1,10 +1,37 @@
+// Create express app 
 const express = require('express')
-
 const app = express()
+const db = require('./db.js')
 
+// Server Information
 const hostname = '127.0.0.1';
 const port = 3000;
 
+// Root endpoint
 app.get('/', (req, res) => {res.send('Hello there!')})
 
-app.listen(8080, () => console.log('Example app listening on port ' + port + '!'))
+
+// API endpoints
+app.get('/entries', (req, res, next) => {
+    var sql = "select * from Entry"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if(err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    }
+    )
+});
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
+
+// Start Server
+app.listen(port, () => console.log('[+] Example app listening on port ' + port + '!'))
---
+++ node/db.js
@@ -0,0 +1,37 @@
+// Create SQLite database 
+const sqlite3 = require('sqlite3').verbose();
+
+// Database information
+const DBSOURCE = "db.sqlite"
+
+// Start SQLite database
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('[+] Connected to the SQLite database.')
+        db.run(`CREATE TABLE Entry (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            entry text, 
+            CONSTRAINT id_unique UNIQUE (id)
+            )`,
+            console.log('[+] Build Table Entry'),
+        (err) => {
+            if (err) {
+                // Table already created
+                console.log('[-] A problem occured - ' + err.message)
+            }else{
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO Entry (entry) VALUES (?)'
+                db.run(insert, "First ToDo Entry")
+                db.run(insert, "Second ToDo Entry")
+                console.log('[+] Inserted first and second row')
+            }
+        });  
+    }
+});
+
+
+module.exports = db
